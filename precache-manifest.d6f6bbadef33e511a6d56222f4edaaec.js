cc5093d08f250d3be2e20e2f2775fe2cd54f48eb
Katherine Le <14ttul@queensu.ca>
2019-08-28 17:00:19 -0400

    Updates

---
+++ precache-manifest.d6f6bbadef33e511a6d56222f4edaaec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ba88f96acf26b6e7c6d724888322967",
+    "url": "/Porfolio/index.html"
+  },
+  {
+    "revision": "3d73458fba1f230f6193",
+    "url": "/Porfolio/static/css/2.26b5c6c4.chunk.css"
+  },
+  {
+    "revision": "052a0f368154e1748cef",
+    "url": "/Porfolio/static/css/main.8beffb24.chunk.css"
+  },
+  {
+    "revision": "3d73458fba1f230f6193",
+    "url": "/Porfolio/static/js/2.68676e73.chunk.js"
+  },
+  {
+    "revision": "052a0f368154e1748cef",
+    "url": "/Porfolio/static/js/main.a5249d31.chunk.js"
+  },
+  {
+    "revision": "4d5da095675b03bf16b8",
+    "url": "/Porfolio/static/js/runtime~main.2fbd1d55.js"
+  },
+  {
+    "revision": "9be330f0f857b47ae277341bea4f0f1e",
+    "url": "/Porfolio/static/media/IMG_0390.9be330f0.JPG"
+  },
+  {
+    "revision": "ba7fb4e45cd511c7630809deed348da5",
+    "url": "/Porfolio/static/media/Katherine-Thanh-Le-Resume.ba7fb4e4.pdf"
+  }
+]);
